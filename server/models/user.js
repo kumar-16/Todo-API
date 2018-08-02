b4ce465742c6f711f6066580249424ee0f6e246a
Aman Kumar <amankumar2186@gmail.com>
2018-08-02 17:29:15 +0530

    Adding Login route

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -68,6 +69,41 @@ UserSchema.statics.findByToken = function (token) {
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.acces': 'auth'
+    });
+};
+
+UserSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    }else{
+        next();
+    }
+});
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+    if(!user){
+        return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, result) => {
+            if(result){
+                resolve(user);
+            }else{
+                reject();
+            }
+        });
+    });
     });
 };
 
